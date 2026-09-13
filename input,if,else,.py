4ce06f5eb10b0053b0e8a03c1d0e944dd631a905
Abhinav K S <abhinavsambhu7@gmail.com>
2026-09-13 10:11:49 +0530

    Create input,if,else,.py

---
+++ input,if,else,.py
@@ -0,0 +1,7 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+if number1 > number2:
+  print("Largest number is", number1)
+else:
+  print("Largest number is", number2)
